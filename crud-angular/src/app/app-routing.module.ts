f0ba905c1f58e1cef89d7331c57040eb9071707f
Thassya Abreu <thassya__@hotmail.com>
2024-03-22 10:37:02 -0300

    criando rota para /courses

--- crud-angular/src/app/app-routing.module.ts
+++ crud-angular/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//path: '' -> acessar a aplicacao num caminho vazio vou mandar abrir courses.
+//pathMatch: 'full' -> vai verificar se n tem nada extra alem do caminho. a barra não é um problema
+//agora vamos adicionar um lazyloading para cursos.
+//consultamos a documentação
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'courses' },
+  {
+    path: 'courses',
+    loadChildren: () => import('./courses/courses.module').then(m=>m.CoursesModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- crud-angular/src/app/courses/courses-routing.module.ts
+++ crud-angular/src/app/courses/courses-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CoursesComponent } from './courses/courses.component';
+
+const routes: Routes = [
+  { path: '', component: CoursesComponent } //dentro de courses, vai ser um caminho em branco e vai chamar o component que criamos agora.
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

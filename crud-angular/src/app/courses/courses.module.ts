9c414a4596584f1ee039f004b72c2ac5e8a26569
Thassya Abreu <thassya__@hotmail.com>
2024-04-02 19:32:22 -0300

    Inclusão componente toolbar na página inicial e no módulo de cursos.

--- crud-angular/src/app/courses/courses.module.ts
+++ crud-angular/src/app/courses/courses.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
@@ -14,7 +15,8 @@ import { CoursesComponent } from './courses/courses.component';
     CommonModule,
     CoursesRoutingModule,
     MatTableModule,
-    MatCardModule
+    MatCardModule,
+    MatToolbarModule
   ]
 })
 export class CoursesModule { }

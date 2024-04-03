883756cf4919e2b7fd834cca1f9b374d2d124554
Thassya Abreu <thassya__@hotmail.com>
2024-04-03 20:25:06 -0300

    chamada http get no angular usando operadores do rxjs

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CoursesService } from './../services/courses.service';
 
 import { Course } from '../model/course';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses', //selector, diretiva. o que poderemos usar no html (semelhante ao <mat-toolbar></mat-toolbar>)
@@ -12,12 +13,13 @@ import { Course } from '../model/course';
 //como usar o servico aqui? atraves da DI, dependency injection
 export class CoursesComponent implements OnInit {
 
-  cursos: Course[] = [];
+  cursos: Observable<Course[]>;
   displayedColumns =['name', 'category'];
 
   constructor(private coursesService: CoursesService){
     this.cursos = this.coursesService.list();
   }
+
   ngOnInit(): void {
 
   }
--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Course } from '../model/course';
 import { HttpClient } from '@angular/common/http';
+import { tap, first } from 'rxjs/operators';
 
 //usaremos o servico dentro do componente.
 //também manipularemos os dados para que seja enviado
@@ -11,6 +12,9 @@ import { HttpClient } from '@angular/common/http';
 })
 export class CoursesService {
 
+  //caminho end-point do servidor, por hora é um arquivo
+  private readonly API = '../../../assets/courses.json';
+
   //queremos obter os dados de uma api que iremos construir, pra isso
   //precisamos fazer requisiçoes ajax (para uma api)
   //uma chamada assincrona para o servidor
@@ -18,10 +22,13 @@ export class CoursesService {
   //para fazermos essa conecao com a api: HTTPCLIENT
   constructor(private httpClient: HttpClient) { }
 
-  list(): Course[] {
-    return [
-      {_id: '1', name: 'Angular', category: 'front-end'}
-    ]
+  list() {
+    return this.httpClient.get<Course[]>(this.API)
+    .pipe( //pipe = cano. manipular a informação de forma reativa passando pelo cano. (com operadores rxjs)
+      //take(1),
+      first(), //finaliza a conexão
+      tap(listaCursos => console.log(listaCursos))
+    );
   };
 
 }
